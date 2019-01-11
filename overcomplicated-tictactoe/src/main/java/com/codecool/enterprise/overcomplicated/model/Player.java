625ff77eef09319e3b3e276285a97c0d2aabc43d
helgahorvath <h.horvath302@gmail.com>
2019-01-11 11:09:56 +0100

    annotations added for player

--- overcomplicated-tictactoe/src/main/java/com/codecool/enterprise/overcomplicated/model/Player.java
+++ overcomplicated-tictactoe/src/main/java/com/codecool/enterprise/overcomplicated/model/Player.java
@@ -1,12 +1,29 @@
 package com.codecool.enterprise.overcomplicated.model;
 
+import org.springframework.stereotype.Component;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.net.URI;
 
+@Entity
+@Component
 public class Player {
+
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    Integer id;
+
     String userName = "Anonymous";
 
     public String getUserName() {
         return userName;
+    }
+
+    public Integer getId() {
+        return id;
     }
 
     public void setUserName(String userName) {
