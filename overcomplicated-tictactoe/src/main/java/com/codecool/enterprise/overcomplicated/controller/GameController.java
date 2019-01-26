522216bcb33af876527b8143479b1f56fe142ff1
helgahorvath <h.horvath302@gmail.com>
2019-01-26 16:01:24 +0100

    GameController implementation

--- overcomplicated-tictactoe/src/main/java/com/codecool/enterprise/overcomplicated/controller/GameController.java
+++ overcomplicated-tictactoe/src/main/java/com/codecool/enterprise/overcomplicated/controller/GameController.java
@@ -6,9 +6,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Controller
 @SessionAttributes({"player", "game"})
 public class GameController {
+
+
+    private TictactoeGame game;
+    private int step;
 
     @ModelAttribute("player")
     public Player getPlayer() {
@@ -17,7 +24,11 @@ public class GameController {
 
     @ModelAttribute("game")
     public TictactoeGame getGame() {
-        return new TictactoeGame();
+        this.game = new TictactoeGame();
+        game.fillFields();
+        step = 0;
+        return this.game;
+
     }
 
     @ModelAttribute("avatar_uri")
@@ -37,14 +48,23 @@ public class GameController {
 
     @GetMapping(value = "/game")
     public String gameView(@ModelAttribute("player") Player player, Model model) {
+        getGame();
         model.addAttribute("funfact", "&quot;Chuck Norris knows the last digit of pi.&quot;");
         model.addAttribute("comic_uri", "https://imgs.xkcd.com/comics/bad_code.png");
         return "game";
     }
 
-    @GetMapping(value = "/game-move")
-    public String gameMove(@ModelAttribute("player") Player player, @ModelAttribute("move") int move) {
-        System.out.println("Player moved " + move);
-        return "redirect:/game";
+    @PostMapping(value = "/game-move")
+    @ResponseBody
+    public Map<String, String> gameMove(@ModelAttribute("player") Player player, @RequestBody String square) {
+        Integer squareId = Integer.parseInt(square.substring(square.length() - 2, square.length() - 1));
+        step++;
+        String sign = step % 2 == 0? "X": "O";
+        game.updateField(squareId, sign);
+        Map<String, String> response = new HashMap<>();
+        response.put("sign", sign);
+        String won = game.isGameWon(sign)? "yes":"no";
+        response.put("won", won);
+        return response;
     }
 }
