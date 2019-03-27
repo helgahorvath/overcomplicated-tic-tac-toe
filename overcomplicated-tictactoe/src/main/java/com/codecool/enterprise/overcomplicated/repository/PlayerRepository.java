2e477060cf41f49c78c484171368b06aea388056
helgahorvath <h.horvath302@gmail.com>
2019-03-27 13:45:35 +0100

    repository removed

--- overcomplicated-tictactoe/src/main/java/com/codecool/enterprise/overcomplicated/repository/PlayerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.codecool.enterprise.overcomplicated.repository;
-
-import com.codecool.enterprise.overcomplicated.model.Player;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PlayerRepository extends JpaRepository<Player, Integer> {
-}
